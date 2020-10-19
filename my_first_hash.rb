da0d5c79f41c8cdd986bdb7290e12ace1d562034
Mason Phillips <mjphillips2000@gmail.com>
2020-10-19 18:43:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,8 +26,8 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  
                  }
-
 
 end
